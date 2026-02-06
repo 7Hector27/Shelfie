f3bc24258cc45899636ac51c8d4c31cfa389a4b1
Hector Carbajal <hector27c7@gmail.com>
2026-02-06 12:30:22 -0800

    finished styling book page

--- src/components/BookContent/BookContent.tsx
+++ src/components/BookContent/BookContent.tsx
@@ -55,39 +55,74 @@ const BookContent = () => {
 
   const authorId = bookData?.authors?.[0]?.author?.key;
   const { title, description } = bookData || {};
+  const coverId = bookData?.covers?.[0];
+  const bookImageUrl = coverId
+    ? `https://covers.openlibrary.org/b/id/${coverId}-L.jpg`
+    : null;
   const { data: authorData, isLoading: isLoadingAuthor } = useQuery({
     queryKey: ["author", authorId],
     queryFn: () => fetchAuthor(authorId as string),
     enabled: !!authorId,
   });
+
   const {
     bio: authorBio,
     birth_date: authorBirthDate,
     name: authorName,
     photos: authorPhotos,
   } = authorData || {};
-  if (isLoading) return <p>Loading…</p>;
+  const authorPhotoId = authorPhotos?.[0];
+  const authorImageUrl = authorPhotoId
+    ? `https://covers.openlibrary.org/b/id/${authorPhotoId}-L.jpg`
+    : null;
+
+  if (isLoading || isLoadingAuthor) return <p>Loading…</p>;
   if (isError) return <p>{(error as Error).message}</p>;
   console.log(bookData);
   console.log(authorData, "authorData");
   return (
     <Layout>
       <div className={styles.bookContent}>
-        <div className={styles.bookDetails}>
-          <Image
-            src={`https://covers.openlibrary.org/b/id/${bookData?.covers?.[0]}-L.jpg`}
-            alt="Book Cover"
-            width={200}
-            height={300}
-          />
-          <h2> {title}</h2>
-          <p>{authorData?.name}</p>
-        </div>
-        <div className={`${styles.buttons} ${styles.want}`}>
-          <button className={styles.actionBtn}>Want to Read</button>
-          <button className={styles.dropdownBtn}>▼</button>
-        </div>
+        <div className={styles.bookDetailsWrapper}>
+          {bookImageUrl ? (
+            <Image
+              src={bookImageUrl}
+              alt="Book Cover"
+              width={200}
+              height={300}
+              unoptimized
+            />
+          ) : (
+            <Image
+              src="/images/book-placeholder.webp"
+              alt="Book Cover"
+              width={200}
+              height={300}
+            />
+          )}
 
+          <div className={styles.bookDetails}>
+            <h2> {title}</h2>
+            <p>{authorData?.name}</p>
+            <div className={styles.ratingTop}>
+              <div className={styles.starsRow}>
+                <div className={styles.stars}>
+                  <span className={styles.filled}>★</span>
+                  <span className={styles.filled}>★</span>
+                  <span className={styles.filled}>★</span>
+                  <span className={styles.filled}>★</span>
+                  <span className={styles.empty}>★</span>
+                </div>
+
+                <span className={styles.ratingNumber}>4.09</span>
+              </div>
+            </div>
+            <div className={`${styles.buttons} ${styles.want}`}>
+              <button className={styles.actionBtn}>Want to Read</button>
+              <button className={styles.dropdownBtn}>▼</button>
+            </div>
+          </div>
+        </div>
         <div
           className={`${styles.description} ${
             isDescriptionExpanded ? styles.expanded : ""
@@ -102,7 +137,6 @@ const BookContent = () => {
             {isDescriptionExpanded ? "Read less" : "Read more"}
           </button>
         </div>
-
         <div className={styles.shelves}>
           <div>
             <p className={styles.count}>1248</p>
@@ -123,10 +157,11 @@ const BookContent = () => {
           <div className={styles.authorDetails}>
             <div className={styles.authorPhoto}>
               <Image
-                src={`https://covers.openlibrary.org/b/id/${authorPhotos?.[0]}-L.jpg`}
+                src={authorImageUrl || "/images/bopk-placeholder.webp"}
                 alt="Author Photo"
                 width={100}
                 height={150}
+                unoptimized={!!authorImageUrl}
               />
             </div>
 
@@ -135,7 +170,7 @@ const BookContent = () => {
               <p>{authorBirthDate}</p>
             </div>
           </div>
-          <p>{authorBio?.value}</p>
+          <p className={styles.authorBio}>{authorBio?.value ?? authorBio}</p>
 
           <button
             className={styles.readMore}
